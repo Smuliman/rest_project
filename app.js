9d197e786d33b3811be58c216540b3214157024c
Smulimaan <samuli.tyyska@live.com>
2022-05-01 22:51:32 +0300

    rest api working

--- app.js
+++ app.js
@@ -1,60 +1,83 @@
 var express = require("express");
-const { FindCursor } = require("mongodb");
+/* const { FindCursor } = require("mongodb"); */
+var mongoose = require("mongoose");
 var app = express();
-const MongoClient = require("mongodb").MongoClient;
+/* const MongoClient = require("mongodb").MongoClient; */
 const port = process.env.PORT || 3000;
-
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const password = "Testi2022";
 
 const uri =
-  "mongodb+srv://MongoSmuli:" +
-  password +
-  "@cluster1.jwcch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://MongoSmuli:" + password + "@cluster1.jwcch.mongodb.net/goals";
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    uri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(" Mongoose is connected")
+  );
+} catch (e) {
+  console.log("could not connect");
+}
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+var Schema = mongoose.Schema;
+
+var goalSchema = new Schema({
+  title: { type: String, required: true, minlength: 3, maxlength: 30 },
+  content: { type: String, required: true, minlength: 10, maxlength: 1000 },
 });
 
-var query = {
-  title: "/Jedi/i",
-};
+var GoalData = mongoose.model("GoalData", goalSchema);
 
-/* client.connect((err) => {
-  const collection = client.db("sample_mflix").collection("movies");
-  if (err) throw err;
-  collection
-    .find(query)
-    .limit(5)
-    .toArray(function (err, result) {
-      if (err) throw err;
-      console.log(result);
-      client.close();
-    });
-}); */
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("Etusivu REST");
+  res.send(
+    "This is an app that lets you save and moderate your goals in life!"
+  );
 });
 
 app.get("/api/getall", function (req, res) {
-  res.send("tulostetaan kaikki tavoitteet");
+  GoalData.find({}, function (err, results) {
+    res.json(results, 200);
+  });
+});
+app.get("/api/:id", function (req, res) {
+  GoalData.findOne({ _id: req.params.id }).then(function (goals) {
+    res.send(goals);
+  });
 });
 
-app.get("/api/:id", function (req, res) {
-  res.send("Muokataan tavoitetta id");
+/* app.get("/api/:id", function (req, res) {
+  res.send("Get spesific goal using goal id");
+}); */
+app.post("/api/add", function (req, res) {
+  var newGoal = new GoalData({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newGoal.save(function (err, goals) {
+    if (err) return console.log(err);
+    console.log(goals);
+  });
+  res.send("new goal added: " + req.body.title + " and " + req.body.content);
+  /* res.send("Add a new goal: " + req.body.title + " (" + req.body.text + ")"); */
 });
-app.get("/api/add", function (req, res) {
-  res.send("lisätään leffa: " + req.body.title + " (" + req.body.text + ")");
+
+app.put("/api/update/:id", function (req, res) {
+  GoalData.findByIdAndUpdate({ _id: req.params.id }, req.body).then(
+    function () {
+      GoalData.findOne({ _id: req.params.id }).then(function (goals) {
+        res.send(goals);
+      });
+    }
+  );
 });
-app.get("/api/update/:id", function (req, res) {
-  res.send("muokataan tavoitetta id: llä: " + req.params.id);
-});
-app.get("/api/delete/:id", function (req, res) {
-  res.send("poistetaan tavoite id: llä: " + req.params.id);
+app.delete("/api/delete/:id", function (req, res) {
+  GoalData.findByIdAndDelete({ _id: req.params.id }).then(function (goals) {
+    res.send(goals);
+  });
 });
 
 app.get("*", function (req, res) {
